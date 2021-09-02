6188974ac640d045c2bddff736452a17c34b02ca
smcla1 <scott.mclamb@gmail.com>
2021-09-02 16:03:45 -0600

    rm imports

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -4,14 +4,11 @@ import { useRef, useState, useCallback } from "react";
 import styles from "./App.styl.js";
 import { withStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Container from "@material-ui/core/Container";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import { Editor, DrawPolygonMode, EditingMode } from "react-map-gl-draw";
 
-import ControlPanel from "../../components/ControlPanel/ControlPanel";
 import MapGL, {
   NavigationControl,
   FullscreenControl,
